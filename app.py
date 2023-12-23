d95034bc1ced052c5a8bb53fbac67b2984383dd6
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-23 22:45:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,22 +3,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error
-import numpy as np
 
-# Function to load and preprocess data
+# Load data
+@st.cache
 def load_data(file_path):
-    data = pd.read_excel(file_path)
-    return data
+    return pd.read_excel(file_path)
 
-# Function to merge economic data with company-specific data
-def merge_data(company_data, economic_data, macro_data):
-    merged_data = pd.merge(company_data, economic_data, on='Date', how='left')
+# Merge data
+def merge_data(company_data, iip_data, macro_data):
+    merged_data = pd.merge(company_data, iip_data, on='Date', how='left')
     merged_data = pd.merge(merged_data, macro_data, on='Reporting Date', how='left')
     return merged_data
 
-# Function to train and evaluate regression models
-def train_models(X_train, y_train, X_test, y_test):
+# Train models
+def train_models(X_train, y_train):
     models = {
         'Linear Regression': LinearRegression(),
         'Random Forest Regression': RandomForestRegressor(),
@@ -29,13 +27,11 @@ def train_models(X_train, y_train, X_test, y_test):
 
     for name, model in models.items():
         model.fit(X_train, y_train)
-        predictions = model.predict(X_test)
-        mse = mean_squared_error(y_test, predictions)
-        results[name] = {'model': model, 'mse': mse}
+        results[name] = model
 
     return results
 
-# Function to make predictions
+# Make predictions
 def make_predictions(model, input_data):
     return model.predict([input_data])
 
@@ -44,13 +40,12 @@ def main():
     st.title("Revenue Prediction Dashboard")
 
     # Upload files
-    st.sidebar.header("Upload Files")
-    company_file = st.sidebar.file_uploader("Upload Company Data (xlsx)", type=["xlsx"])
-    iip_file = st.sidebar.file_uploader("Upload IIP Data (csv)", type=["csv"])
-    macro_file = st.sidebar.file_uploader("Upload Macro Data (xlsx)", type=["xlsx"])
+    company_file = st.file_uploader("Upload Company Data (xlsx)", type=["xlsx"])
+    iip_file = st.file_uploader("Upload IIP Data (csv)", type=["csv"])
+    macro_file = st.file_uploader("Upload Macro Data (xlsx)", type=["xlsx"])
 
-    # Load data
     if company_file is not None and iip_file is not None and macro_file is not None:
+        # Load data
         company_data = load_data(company_file)
         iip_data = load_data(iip_file)
         macro_data = load_data(macro_file)
@@ -63,16 +58,11 @@ def main():
 
         # Train-test split
         X = merged_data[selected_columns].dropna()
-        y = merged_data['Total Revenue/Income'].loc[X.index]
+        y = X.pop('Total Revenue/Income')
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         # Train models
-        results = train_models(X_train, y_train, X_test, y_test)
-
-        # Display model results
-        st.subheader("Model Results")
-        for name, result in results.items():
-            st.write(f"{name} MSE: {result['mse']}")
+        models = train_models(X_train, y_train)
 
         # User input for prediction
         st.sidebar.header("Make Predictions")
@@ -81,12 +71,11 @@ def main():
             input_data[column] = st.sidebar.number_input(f"Enter value for {column}", min_value=0)
 
         # User selects model for prediction
-        selected_model = st.sidebar.selectbox("Select Model for Prediction", list(results.keys()))
-        selected_model = results[selected_model]['model']
+        selected_model = st.sidebar.selectbox("Select Model for Prediction", list(models.keys()))
 
         # Make predictions
         if st.sidebar.button("Make Predictions"):
-            prediction = make_predictions(selected_model, input_data)
+            prediction = make_predictions(models[selected_model], input_data)
             st.subheader("Predicted Total Revenue/Income")
             st.write(f"The predicted value is: {prediction[0]}")
 
