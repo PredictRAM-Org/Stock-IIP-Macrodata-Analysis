1dde861e1bf87cd1de2fa62a38627cfc940ea3ae
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-23 23:19:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,67 +13,70 @@ macro_data = pd.read_excel('macro_data.xlsx')
 # Step 2: Read IIP.csv file
 iip_data = pd.read_csv('IIP.csv')
 
-# Step 3: Read stock data file
-stock_data = pd.read_excel('stock_data.xlsx')
-
-# Step 4: Create a Streamlit app
+# Step 3: Create a Streamlit app
 st.title("Economic Indicators Predictive Model")
 
-# Step 5: Add widgets for user input
+# Step 4: Add widgets for user input
 selected_macro_columns = st.multiselect('Select columns from macro_data.xlsx:', macro_data.columns)
 selected_iip_columns = st.multiselect('Select columns from IIP.csv:', iip_data.columns)
-selected_stock_columns = st.multiselect('Select columns from stock_data.xlsx:', stock_data.columns)
 
-# Step 6: Train predictive models (Linear Regression, Random Forest Regression, Gradient Boosting Regression)
-# Example for macro_data
-macro_selected_data = macro_data[selected_macro_columns]
-macro_target = stock_data['Total Revenue/Income']
+# Step 5: Upload stock_data file
+uploaded_stock_data = st.file_uploader("Upload stock_data Excel file", type=["xlsx", "xls"])
 
-# Split data into training and testing sets
-macro_train_data, macro_test_data, macro_train_target, macro_test_target = train_test_split(
-    macro_selected_data, macro_target, test_size=0.2, random_state=42
-)
+# Check if a file was uploaded
+if uploaded_stock_data is not None:
+    # Read uploaded stock_data file
+    stock_data = pd.read_excel(uploaded_stock_data)
 
-# Linear Regression
-linear_model = LinearRegression()
-linear_model.fit(macro_train_data, macro_train_target)
-linear_predictions = linear_model.predict(macro_test_data)
+    # Step 6: Train predictive models (Linear Regression, Random Forest Regression, Gradient Boosting Regression)
+    # Example for macro_data
+    macro_selected_data = macro_data[selected_macro_columns]
+    macro_target = stock_data['Total Revenue/Income']
 
-# Random Forest Regression
-rf_model = RandomForestRegressor()
-rf_model.fit(macro_train_data, macro_train_target)
-rf_predictions = rf_model.predict(macro_test_data)
+    # Split data into training and testing sets
+    macro_train_data, macro_test_data, macro_train_target, macro_test_target = train_test_split(
+        macro_selected_data, macro_target, test_size=0.2, random_state=42
+    )
 
-# Gradient Boosting Regression
-gb_model = GradientBoostingRegressor()
-gb_model.fit(macro_train_data, macro_train_target)
-gb_predictions = gb_model.predict(macro_test_data)
+    # Linear Regression
+    linear_model = LinearRegression()
+    linear_model.fit(macro_train_data, macro_train_target)
+    linear_predictions = linear_model.predict(macro_test_data)
 
-# Step 7: Compare models with income statement data
-# Compare models using Mean Squared Error
-linear_mse = mean_squared_error(macro_test_target, linear_predictions)
-rf_mse = mean_squared_error(macro_test_target, rf_predictions)
-gb_mse = mean_squared_error(macro_test_target, gb_predictions)
+    # Random Forest Regression
+    rf_model = RandomForestRegressor()
+    rf_model.fit(macro_train_data, macro_train_target)
+    rf_predictions = rf_model.predict(macro_test_data)
 
-st.write(f'Linear Regression Mean Squared Error: {linear_mse}')
-st.write(f'Random Forest Regression Mean Squared Error: {rf_mse}')
-st.write(f'Gradient Boosting Regression Mean Squared Error: {gb_mse}')
+    # Gradient Boosting Regression
+    gb_model = GradientBoostingRegressor()
+    gb_model.fit(macro_train_data, macro_train_target)
+    gb_predictions = gb_model.predict(macro_test_data)
 
-# Step 8: Add input options for upcoming values
-upcoming_iip_data = st.text_input('Enter upcoming values for selected IIP.csv columns:')
-upcoming_macro_data = st.text_input('Enter upcoming values for selected macro_data.xlsx columns:')
+    # Step 7: Compare models with income statement data
+    # Compare models using Mean Squared Error
+    linear_mse = mean_squared_error(macro_test_target, linear_predictions)
+    rf_mse = mean_squared_error(macro_test_target, rf_predictions)
+    gb_mse = mean_squared_error(macro_test_target, gb_predictions)
 
-# Step 9: Predict upcoming total revenue/income, total operating expense, EBITDA, and Net income
-# Example for predicting upcoming values
-upcoming_iip_data = pd.read_csv(StringIO(upcoming_iip_data))
-upcoming_macro_data = pd.read_excel(StringIO(upcoming_macro_data))
+    st.write(f'Linear Regression Mean Squared Error: {linear_mse}')
+    st.write(f'Random Forest Regression Mean Squared Error: {rf_mse}')
+    st.write(f'Gradient Boosting Regression Mean Squared Error: {gb_mse}')
 
-# Use trained models to predict upcoming values
-upcoming_macro_predictions = linear_model.predict(upcoming_macro_data)
+    # Step 8: Add input options for upcoming values
+    upcoming_iip_data = st.text_input('Enter upcoming values for selected IIP.csv columns:')
+    upcoming_macro_data = st.text_input('Enter upcoming values for selected macro_data.xlsx columns:')
 
-# Step 10: Display comparative chart
-# Code for displaying chart goes here
+    # Step 9: Predict upcoming total revenue/income, total operating expense, EBITDA, and Net income
+    # Example for predicting upcoming values
+    upcoming_iip_data = pd.read_csv(StringIO(upcoming_iip_data))
+    upcoming_macro_data = pd.read_excel(StringIO(upcoming_macro_data))
 
-# Optionally, you can display the predicted values
-st.write("Predicted Total Revenue/Income:", upcoming_macro_predictions)
+    # Use trained models to predict upcoming values
+    upcoming_macro_predictions = linear_model.predict(upcoming_macro_data)
 
+    # Step 10: Display comparative chart
+    # Code for displaying chart goes here
+
+    # Optionally, you can display the predicted values
+    st.write("Predicted Total Revenue/Income:", upcoming_macro_predictions)
